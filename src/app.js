5112f55afcaea48a36cb0bb207f3cc34227c88aa
Burak <48869708+drementer@users.noreply.github.com>
2024-05-12 20:51:29 +0300

    chore: Update dependencies and add express server

--- src/app.js
+++ src/app.js
@@ -1,28 +1,42 @@
-const axios = require("axios");
-const HTMLParser = require("node-html-parser");
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const baseUrl = "https://kilincarslan.dev/";
+const axios = require('axios');
+const HTMLParser = require('node-html-parser');
+
+const baseUrl = 'https://kilincarslan.dev/';
+const visitedLinks = [];
 
 const getPage = async (url) => {
+  if (url.startsWith('/')) url = baseUrl + url;
+
   const page = await axios.get(url);
   return page.data;
 };
 
 const getLinks = (html) => {
   const root = HTMLParser.parse(html);
-  const links = root.querySelectorAll("a");
-  return links.map((link) => link.getAttribute("href"));
+  const links = root.querySelectorAll('a');
+  return links.map((link) => link.getAttribute('href'));
 };
 
 const listLinks = async (url) => {
+  if (visitedLinks.includes(url)) return;
+
   const page = await getPage(url);
-  return getLinks(page);
+  const links = getLinks(page);
+
+  visitedLinks.push(url);
+  for (const link of links) await listLinks(link);
 };
 
-try {
-  listLinks(baseUrl).then((links) => {
-    console.log(links);
-  });
-} catch (error) {
-  console.log("📌 ~ error ->", error.message);
-}
+app.get('/', async (req, res) => {
+  await listLinks(baseUrl);
+  console.log('📌 ~ visitedLinks ->', visitedLinks);
+  res.status(200).send(visitedLinks);
+});
+
+app.listen(port, () => {
+  console.log(`APP ON: https://localhost:${port}`);
+});
