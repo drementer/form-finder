1e800a0cf3eb539b28088497448e62899ff2008f
drementer <48869708+drementer@users.noreply.github.com>
2024-05-19 18:03:23 +0300

    Vercel test

--- api/app.js
+++ api/app.js
@@ -4,7 +4,12 @@ const router = require('./router');
 const app = express();
 const port = 3001;
 
-app.use('/api/', router);
+const timeLog = (req, res, next) => {
+  console.log('Time: ', Date.now());
+  res.status(200).send('Time: ' + Date.now());
+};
+
+app.use(timeLog);
 
 app.listen(port, () => console.log(`APP ON: http://localhost:${port}`));
 
