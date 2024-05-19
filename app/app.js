71b62dc33896ea651ffc6dc55b3ca812977cd1ab
drementer <48869708+drementer@users.noreply.github.com>
2024-05-19 16:20:32 +0300

    refactor: delete logging and update readable

--- app/app.js
+++ app/app.js
@@ -1,15 +1,9 @@
 const express = require('express');
+const router = require('./router');
+
 const app = express();
 const port = 3001;
 
-const router = require('./router');
-
-const timeLog = (req, res, next) => {
-  console.log('Time: ', Date.now());
-  next();
-};
-
-app.use(timeLog);
 app.use('/api/', router);
 
 app.listen(port, () => console.log(`APP ON: http://localhost:${port}`));
