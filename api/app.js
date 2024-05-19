a91ee624e7f4f827e7b634b6fee548fde4eab29a
drementer <48869708+drementer@users.noreply.github.com>
2024-05-19 18:05:32 +0300

    Test: delete subpages for vercel test

--- api/app.js
+++ api/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const router = require('./router');
 
 const app = express();
 const port = 3001;
--- api/router/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const pages = require('./pages');
-
-router.use('/pages', pages);
-
-module.exports = router;
