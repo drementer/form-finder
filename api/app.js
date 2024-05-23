14dbf4d8d183718483295b67628d32dd433561c2
drementer <48869708+drementer@users.noreply.github.com>
2024-05-23 23:35:52 +0300

    chore: add CORS headers to API

--- api/app.js
+++ api/app.js
@@ -4,6 +4,12 @@ const router = require('./router');
 const app = express();
 const port = 3001;
 
+// add cors headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use('/api/', router);
 
 app.listen(port, () => console.log(`APP ON: http://localhost:${port}`));
