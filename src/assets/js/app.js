0c0bb52df06fb79c90a8e17d55e2429433fe950f
drementer <48869708+drementer@users.noreply.github.com>
2024-05-23 23:39:12 +0300

    Feat: adding site search scripts

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,35 @@
-console.log('Hello World');
+const urlParams = new URLSearchParams(window.location.search);
+const searchUrl = urlParams.get('url');
+
+const searchBox = document.querySelector('[search-box]');
+
+const isValidUrl = (url) => {
+  const urlRegex = /^(http|https):\/\/[^ "]+$/;
+  return urlRegex.test(url);
+};
+
+const getSiteData = async (url) => {
+  try {
+    const response = await axios.get(`/api/pages?site=${url}`);
+    return response.data;
+  } catch (error) {
+    console.error(`Failed to get site data: ${error}`);
+  }
+};
+
+const filterUrl = (url) => {
+  if (!isValidUrl(url)) url = `https://${url}`;
+
+  return new URL(searchValue).hostname;
+};
+
+if (searchUrl) {
+  searchBox.value = searchUrl;
+  getSiteData(searchUrl);
+}
+
+searchBox.addEventListener('input', () => {
+  const searchValue = searchBox.value;
+
+  if (searchValue) searchBox.value = filterUrl(searchValue);
+});
