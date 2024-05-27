8483e497e7fd2c46413813a88565ca34315533e7
Burak <48869708+drementer@users.noreply.github.com>
2024-05-28 00:47:36 +0300

    Feat search bar styling and functionality

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,11 @@
 const urlParams = new URLSearchParams(window.location.search);
 const searchUrl = urlParams.get('url');
+
+const searchBar = document.querySelector('[search-bar]');
 const searchInput = document.querySelector('[search-input]');
+const resultSection = document.querySelector('[result]');
 const resultList = document.querySelector('[result-list]');
+
 const apiAddress = 'http://localhost:3001/api/pages';
 
 const isValidUrl = (url) => /^(http|https):\/\/[^ "]+$/.test(url);
@@ -9,10 +13,11 @@ const isValidUrl = (url) => /^(http|https):\/\/[^ "]+$/.test(url);
 const createLinkBox = (url) => {
   return Object.assign(document.createElement('a'), {
     href: url,
-    textContent: url,
+    textContent: url.replace(`https://${searchUrl}`, ''),
     target: '_blank',
     rel: 'noopener noreferrer',
     title: 'Open link in a new tab',
+    classList: ['link-box'],
   });
 };
 
@@ -39,6 +44,22 @@ const handleSearch = () => {
 };
 
 searchInput.addEventListener('input', handleSearch);
+
+searchBar.style.top = `${-searchBar.clientHeight * 0.6}px`;
+let lastScroll = 0;
+
+window.addEventListener('resize', () => {
+  searchBar.style.top = `${-searchBar.clientHeight * ((100 / 3) * 2)}px`;
+});
+
+window.addEventListener('scroll', () => {
+  const isResultVisible = resultSection.getBoundingClientRect().top < 0;
+  const isReverseScroll = window.scrollY < lastScroll;
+  lastScroll = window.scrollY;
+
+  if (!isResultVisible) return searchBar.classList.remove('-show');
+  searchBar.classList.toggle('-show', isReverseScroll);
+});
 
 if (searchUrl) {
   const filteredUrl = filterUrl(searchUrl);
