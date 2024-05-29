e6c634730560cda7c2b493c48f6d825dd75fe021
drementer <48869708+drementer@users.noreply.github.com>
2024-05-29 23:10:43 +0300

    refactor: Remove unused CORS middleware

--- api/app.js
+++ api/app.js
@@ -4,11 +4,6 @@ const router = require('./router');
 const app = express();
 const port = 3001;
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
-
 app.use('/api/', router);
 
 app.listen(port, () => console.log(`APP ON: http://localhost:${port}`));
