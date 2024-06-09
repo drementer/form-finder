3fa3d95f989f2e58c913c2d35b90eda6c0239d2a
drementer <48869708+drementer@users.noreply.github.com>
2024-06-09 19:44:48 +0300

    Feat: Split app file to server and app

--- api/app.js
+++ api/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = require('./router');
 
 const app = express();
-const port = 3001;
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.use('/api/', router);
-
-app.listen(port, () => console.log(`APP ON: http://localhost:${port}`));
 
 module.exports = app;
