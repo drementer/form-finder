d78465a15518ad2bb9ba1a2e61b6b021b4c837f7
drementer <48869708+drementer@users.noreply.github.com>
2024-08-17 23:10:10 +0300

    Feat: form finding logic to return form information

--- api/helpers/findForm.js
+++ api/helpers/findForm.js
@@ -1,22 +1,53 @@
 const inputSelector = 'input:not([type="hidden"], [type="submit"]), textarea, select';
 const submitButtonSelector = 'input[type="submit"], button[type="submit"]';
 
+const getElementInfo = (element) => {
+  const info = {
+    name: element.getAttribute('name'),
+    type: element.getAttribute('type') || element.tagName.toLowerCase(),
+    defaultValue: element.getAttribute('value') || null,
+    placeholder: element.getAttribute('placeholder') || null,
+  };
+
+  if (info.type === 'select') {
+    const selectedOption = element.querySelector('option:selected');
+    const firstOption = element.querySelector('option');
+
+    const firstValue = firstOption?.getAttribute('value');
+    const selectedValue = selectedOption?.getAttribute('value');
+
+    info.defaultValue = selectedValue || firstValue;
+  }
+
+  return info;
+};
+
 const checkFormValidity = (form) => {
   const inputs = form.querySelectorAll(inputSelector);
   const submitButton = form.querySelector(submitButtonSelector);
 
   const haveMultipleInputs = inputs.length > 1;
   const haveSubmitButton = !!submitButton;
+  const isValidForm = haveMultipleInputs && haveSubmitButton;
 
-  return haveMultipleInputs && haveSubmitButton;
+  if (!isValidForm) return;
+
+  return {
+    name: form.getAttribute('name') || null,
+    action: form.getAttribute('action'),
+    method: form.getAttribute('method'),
+    elements: inputs.map(getElementInfo),
+  };
 };
 
 const findForm = (html) => {
   try {
     const forms = html.querySelectorAll('form');
-    const validForms = forms.filter(checkFormValidity);
+    const validForms = [...forms].map(checkFormValidity).filter(Boolean);
 
-    return validForms.length;
+    if (!validForms.length) return false;
+
+    return validForms;
   } catch (error) {
     throw new Error(`Error parsing HTML: ${error.message}`);
   }
