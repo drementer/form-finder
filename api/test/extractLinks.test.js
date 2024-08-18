da3680e7d5d00cdccbc790e2eaee883c8fd46bb2
drementer <48869708+drementer@users.noreply.github.com>
2024-08-18 16:40:43 +0300

    Feat: Add test for extract links module

---
+++ api/test/extractLinks.test.js
@@ -0,0 +1,31 @@
+const { parse } = require('node-html-parser');
+const extractLinks = require('../helpers/extractLinks');
+
+const document = parse(`
+  <a href="https://example.com/1">Full URL</a>
+  <a href="/2">Just Path</a>
+  <a href="/2?parameter">Just Path</a>
+  <a href="/2?parameter#section-1">Just Path</a>
+  <a href="/2#section-1">Just Path</a>
+  <a href="/3 ">Just Path with space</a>
+  <a href="">Empty href</a>
+  <a>NO HREF</a>
+  <div>WHO AM I?</div>
+  <div href="/wtf">WHO AM I?</div>
+`);
+
+const expectedLinks = [
+  'https://example.com/1',
+  '/2',
+  '/2?parameter',
+  '/2?parameter#section-1',
+  '/2#section-1',
+  '/3',
+];
+
+describe('extractLinks', () => {
+  it('should return an array of valid href links', () => {
+    const links = extractLinks(document);
+    expect(links).toEqual(expectedLinks);
+  });
+});
